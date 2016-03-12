1c00c988b1980a595491013217339d838c32ed65
Damian Faustryjak <dfaustryjak@hotmail.com>
2016-03-12 10:45:07 +0100

    12-03-2016

---
+++ Kantor/Pages/Archives.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Kantor.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Archives : Page
+    {
+        public Archives()
+        {
+            this.InitializeComponent();
+        }
+        private void example(Type type)
+        {
+            MySplitView.IsPaneOpen = false;
+            this.Frame.Navigate(type);
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = true;
+            Button b = (Button)sender;
+            b.Foreground = new SolidColorBrush(Windows.UI.Colors.White);
+        }
+        private void btnShowPane_click1(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = false;
+            Button b = (Button)sender;
+            b.Foreground = new SolidColorBrush(Windows.UI.Colors.White);
+            this.Frame.Navigate(typeof(Pages.Current));
+        }
+
+        private void btnShowPane_click2(object sender, RoutedEventArgs e)
+        {
+
+            example(typeof(Pages.Archives));
+            Button b = (Button)sender;
+            b.Foreground = new SolidColorBrush(Windows.UI.Colors.White);
+        }
+    }
+}
